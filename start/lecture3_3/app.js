9445e7ce097c2f280b698a9620ecf6d4e41ca2c3
Jorge Sader <Jorge.Sader@gmail.com>
2023-04-26 08:14:49 -0400

    my lesson 3_3

--- start/lecture3_3/app.js
+++ start/lecture3_3/app.js
@@ -1,21 +1,57 @@
 import * as THREE from '../../libs/three126/three.module.js';
 import { OrbitControls } from '../../libs/three126/OrbitControls.js';
 
-class App{
-	constructor(){
-		const container = document.createElement( 'div' );
-		document.body.appendChild( container );
+class App {
+  constructor() {
+    const container = document.createElement('div');
+    document.body.appendChild(container);
 
-        window.addEventListener('resize', this.resize.bind(this) );
-	}	
-    
-    resize(){
+    this.camera = new THREE.PerspectiveCamera(
+      60,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      100
+    );
+    this.camera.position.set(0, 0, 4);
 
-    }
-    
-	render( ) {   
-		
-    }
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0xaaaaaa);
+
+    this.ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 0.3);
+    this.scene.add(this.ambient);
+
+    const light = new THREE.DirectionalLight();
+    light.position.set(0.2, 1, 1);
+    this.scene.add(light);
+
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(this.renderer.domElement);
+
+    this.renderer.setAnimationLoop(this.render.bind(this));
+
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshStandardMaterial({ color: 0xff0000 });
+
+    this.mesh = new THREE.Mesh(geometry, material);
+    this.scene.add(this.mesh);
+
+    const controls = new OrbitControls(this.camera, this.renderer.domElement);
+    window.addEventListener('resize', this.resize.bind(this));
+  }
+
+  resize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  render() {
+    this.mesh.rotateX(0.01);
+    this.mesh.rotateY(0.01);
+    this.renderer.render(this.scene, this.camera);
+  }
 }
 
 export { App };
